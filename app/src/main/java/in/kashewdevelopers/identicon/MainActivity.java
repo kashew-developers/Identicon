a52f2aa9ca696797177df87e33345341508761e3
kashew-developers <kashewdevelopers@gmail.com>
2020-08-26 15:20:29 +0530

    Update - Add Share button

--- app/src/main/java/in/kashewdevelopers/identicon/MainActivity.java
+++ app/src/main/java/in/kashewdevelopers/identicon/MainActivity.java
@@ -11,17 +11,16 @@ import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -40,7 +39,6 @@ public class MainActivity extends AppCompatActivity {
     Random random;
     ImageView identicon;
     TextView fileLocation;
-    Button save;
     int imageSize, borderSize, numberOfBlocks, blockSize;
     int red, green, blue;
     int WRITE_PERMISSION = 1234;
@@ -48,7 +46,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -59,52 +56,68 @@ public class MainActivity extends AppCompatActivity {
         random = new Random();
         bitmap = Bitmap.createBitmap(imageSize, imageSize, Bitmap.Config.RGB_565);
         identicon = findViewById(R.id.identicon);
-        save = findViewById(R.id.save);
         fileLocation = findViewById(R.id.fileLocation);
 
         drawIdenticon();
-
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options, menu);
+        return true;
+    }
 
-    public void checkPermission(View v) {
-
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) !=
-                PackageManager.PERMISSION_GRANTED) {
-
-            if (!ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-
-                noPermission();
-
-                new AlertDialog.Builder(this)
-                        .setTitle("Need Storage Permission")
-                        .setPositiveButton("Give Permission", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                startActivity(new Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package:" + BuildConfig.APPLICATION_ID)));
-                            }
-                        })
-                        .create()
-                        .show();
-            } else {
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, WRITE_PERMISSION);
-            }
-        } else {
-            save();
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.refreshOption) {
+            drawIdenticon();
+            return true;
         }
-
+        return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == WRITE_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] != PackageManager.PERMISSION_GRANTED)
+                noPermission();
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+
+    // permissions
+    public boolean hasWritePermission() {
+        return (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED);
+    }
+
+    public void askWritePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+
+            new AlertDialog.Builder(this)
+                    .setTitle("Need Storage Permission")
+                    .setPositiveButton("Give Permission", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            startActivity(new Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package:" + BuildConfig.APPLICATION_ID)));
+                        }
+                    })
+                    .create()
+                    .show();
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, WRITE_PERMISSION);
+        }
+    }
 
     public void noPermission() {
         Toast.makeText(this, "Give permission to save file", Toast.LENGTH_LONG).show();
     }
 
 
-    public void save() {
-
+    public File saveIdenticon() {
         String path = Environment.getExternalStorageDirectory().toString();
         OutputStream fOut;
 
@@ -114,26 +127,25 @@ public class MainActivity extends AppCompatActivity {
         format.setTimeZone(calendar.getTimeZone());
         String fileName = format.format(date);
 
-        File file = new File(path, "Identicon");
+        File file = new File(path, getString(R.string.app_name));
         if (file.exists()) {
             int counter = 1;
             while (!file.isDirectory()) {
-                file = new File(path, "Identicon - " + counter);
+                file = new File(path, getString(R.string.app_name) + " - " + counter);
                 counter++;
             }
-        } else {
-            file.mkdir();
+        } else if (!file.mkdir()) {
+            return null;
         }
 
-        file = new File(path, "Identicon/" + fileName + ".png");
+        file = new File(path, getString(R.string.app_name) + "/" + fileName + ".png");
         int counter = 1;
         while (file.exists()) {
-            file = new File(path, "Identicon/" + fileName + " - " + counter + ".png");
+            file = new File(path, getString(R.string.app_name) + "/" + fileName + " - " + counter + ".png");
             counter++;
         }
 
         try {
-
             fOut = new FileOutputStream(file);
 
             bitmap.compress(Bitmap.CompressFormat.PNG, 100, fOut);
@@ -141,22 +153,34 @@ public class MainActivity extends AppCompatActivity {
             fOut.close();
 
             ContentValues contentValues = new ContentValues();
-            contentValues.put(MediaStore.MediaColumns.DATA, file.getAbsolutePath());
+            contentValues.put(MediaStore.Images.Media.DATA, file.getAbsolutePath());
             getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
-
-            fileLocation.setText(getApplicationContext().getString(R.string.fileLocation, file.getAbsolutePath()));
-
         } catch (Exception e) {
-            Toast.makeText(this, "Error! Could'nt save file", Toast.LENGTH_LONG).show();
-            Log.d("KashewDevelopers", "Error : " + e.getMessage());
+            return null;
         }
 
+        return file;
+    }
 
+    public Uri getImageContentUri(File imageFile) {
+        String filePath = imageFile.getAbsolutePath();
+
+        Cursor cursor = getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                new String[]{MediaStore.Images.Media._ID},
+                MediaStore.Images.Media.DATA + "=? ",
+                new String[]{filePath}, null);
+
+        if (cursor != null && cursor.moveToFirst()) {
+            int id = cursor.getInt(cursor.getColumnIndex(MediaStore.MediaColumns._ID));
+            cursor.close();
+            return Uri.withAppendedPath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "" + id);
+        }
+        return null;
     }
 
 
+    // drawing
     public void paint(int x, int y) {
-
         x = borderSize + (x * blockSize);
         y = borderSize + (y * blockSize);
 
@@ -164,15 +188,11 @@ public class MainActivity extends AppCompatActivity {
             for (int j = 0; j < blockSize; j++) {
                 bitmap.setPixel(x + i, y + j, Color.rgb(red, green, blue));
                 bitmap.setPixel(imageSize - x - i, y + j, Color.rgb(red, green, blue));
-
             }
         }
-
     }
 
-
     public void drawIdenticon() {
-
         bitmap.eraseColor(Color.rgb(240, 240, 240));
 
         red = random.nextInt(256);
@@ -191,42 +211,48 @@ public class MainActivity extends AppCompatActivity {
         }
 
         identicon.setImageBitmap(bitmap);
-
     }
 
 
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.options, menu);
-        return true;
+    // handle widget clicks
+    public void refreshClicked(View v) {
+        drawIdenticon();
     }
 
+    public void saveClicked(View v) {
+        if (hasWritePermission()) {
+            File imageFile = saveIdenticon();
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+            if (imageFile != null) {
+                fileLocation.setText(getApplicationContext().getString(R.string.fileLocation,
+                        imageFile.getAbsolutePath()));
+            } else {
+                Toast.makeText(this, "Error! Could'nt save file", Toast.LENGTH_LONG).show();
+            }
 
-        if (item.getItemId() == R.id.refreshOption) {
-            drawIdenticon();
-            return true;
+        } else {
+            askWritePermission();
         }
-
-        return super.onOptionsItemSelected(item);
-
     }
 
+    public void shareClicked(View v) {
+        if (hasWritePermission()) {
+            File imageFile = saveIdenticon();
+            Uri imageUri = (imageFile == null) ? null : getImageContentUri(imageFile);
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+            if (imageUri != null) {
+                Intent intent = new Intent(android.content.Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_STREAM, imageUri);
+                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                intent.setType("image/png");
+                startActivity(Intent.createChooser(intent, "Share With"));
+            } else {
+                Toast.makeText(this, "Error! Could'nt share file", Toast.LENGTH_LONG).show();
+            }
 
-        if (requestCode == WRITE_PERMISSION) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                save();
-            else
-                noPermission();
+        } else {
+            askWritePermission();
         }
-
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
     }
-
 
 }
